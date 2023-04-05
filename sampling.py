62350256cac572f5b0939c9f4f1964beacf0ded8
upp <upptechnlogy@gmail.com>
2023-04-05 17:34:16 +0700

    sample file

--- sampling.py
+++ sampling.py
@@ -21,9 +21,7 @@ def sampling_data(url, saved_url, sampling_size):
     # random
     image_ids = sample(list(image_ids), int(len(image_ids) * sampling_size))
     
-    ans = {}
-    ans['questions'] = []
-    temp = {}
+    ans = []
     for question in data:
         if question['image_id'] in image_ids:
             ans.append(question)
@@ -47,11 +45,11 @@ def read_file_json(filename):
 # TODO: transfer the data -> data used for training
 # annotations include image_id, question_id, answers -> train from this. 
 # answer weights?
-if __name__ == '__main__':
-    for ln in ['train', 'val']:
+def format_annotations():
+    for phase in ['train', 'val']:
         
-        annotations = read_file_json(f'data/v2_mscoco_{ln}2014_annotations.json') # image id, question id, answer
-        questions = read_file_json(f'data/v2_OpenEnded_mscoco_{ln}2014_questions.json') # image id, question id, question
+        annotations = read_file_json(f'data/v2_mscoco_{phase}2014_annotations.json') # image id, question id, answer
+        questions = read_file_json(f'data/v2_OpenEnded_mscoco_{phase}2014_questions.json') # image id, question id, question
         
         save_ann_for_training = []
         for annotation in tqdm(annotations['annotations']):
@@ -64,17 +62,19 @@ if __name__ == '__main__':
                             'question_type': annotation['question_type'],
                             "question" : question['question'],
                             "anwers" : annotation['answers'],
-                            "data_subtype" : ln
+                            "data_subtype" : phase
                         })
 
         
-        with open(f'data/{ln}_formatted.json', 'w') as f:
+        with open(f'data/{phase}_formatted.json', 'w') as f:
             json.dump(save_ann_for_training, f)
 
-        print(f'data/{ln}_formatted is saved!')
+        print(f'data/{phase}_formatted is saved!')
 
 
-
+if __name__ == '__main__':
+    for phase in ['train', 'val']:
+        sampling_data(url=f'data/{phase}_formatted.json', saved_url=f'data/sample_{phase}_formatted.json', sampling_size=0.1)    
 
 
 
